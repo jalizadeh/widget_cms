dd9e848499e4e3feea7ba8c9f8c35fcabbec6a34
JAlizadeh <jalizade3d@yahoo.com>
2017-06-30 16:49:42 -0700

    Functions added

--- includes/functions.php
+++ includes/functions.php
@@ -21,11 +21,11 @@ function findAllSubjects(){
 
 
 // find list of pages based on Subject ID
-function  findPagesForSubject($subjectId){
+function  findPagesForSubject($subject_id){
 	global $connection;
 
 	$query = " SELECT * FROM pages
-				 WHERE subject_id = {$subjectId}
+				 WHERE subject_id = {$subject_id}
 				 AND visible = 1
 				 ORDER BY position ASC";
 	$pages_q = mysqli_query($connection, $query);
@@ -34,15 +34,65 @@ function  findPagesForSubject($subjectId){
 }
 
 
+function findSelectedSubjectById($subject_id){
+	global $connection;
+
+	$safe_id = mysqli_real_escape_string($connection,$subject_id);
+	$query = "SELECT * FROM subjects
+				 WHERE id = {$safe_id}
+				 LIMIT 1";
+	$subject_set = mysqli_connect($connection, $query);
+	confirmQuery($subject_set);
+	if ($subject = mysqli_fetch_assoc($subject_set)){
+		return $subject;
+	} else {
+		return null;
+	}
+}
+
+function findSelectedPageById($page_id){
+	global $connection;
+
+	$safe_id = mysqli_real_escape_string($connection,$page_id);
+	$query = "SELECT * FROM pages
+				 WHERE id = {$safe_id}
+				 LIMIT 1";
+	$page_set = mysqli_connect($connection, $query);
+	confirmQuery($page_set);
+	if ($page = mysqli_fetch_assoc($page_set)){
+		return $page;
+	} else {
+		return null;
+	}
+}
+
+
+function findSelectedSubject_Page(){
+	global $selected_subject_id;
+	global $selected_page_id;
+
+	if(isset($_GET["subject"])){
+		$selected_subject_id = $_GET["subject"];
+		$selected_page_id = null;
+	} elseif (isset($_GET["page"])){
+		$selected_subject_id = null;
+		$selected_page_id = $_GET["page"];
+	} else{
+		$selected_subject_id = null;
+		$selected_page_id = null;
+	}
+}
+
+
 // it shows the navigation menu
 // also puts bullet beside the selected subject/page
-function navigation($subjectId, $pageId){
+function navigation($subject_id, $page_id){
 	$output = "<ul class=\"subjects\">";
 	$subjects = findAllSubjects();
 	while($subject_f = mysqli_fetch_assoc($subjects)){
 		$output .= "<li ";
 		// check to find the selected subject
-		if($subject_f["id"] == $subjectId ){
+		if($subject_f["id"] == $subject_id ){
 			$output .= "class=\"selected\"";
 		} 
 		$output .= ">";
@@ -54,7 +104,7 @@ function navigation($subjectId, $pageId){
 				$output .= "<li ";
 
 			// check to find the selected page
-			if($pages_f["id"] == $pageId ){
+			if($pages_f["id"] == $page_id ){
 				$output .= "class=\"selected\" ";
 			} 
 			$output .= "><a href=\"manage_content.php?page=".urlencode($pages_f["id"])."\">". $pages_f["menu_name"]."</a></li>";
